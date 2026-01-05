8e0f63cfd2cf845ce3ef60c0559badc3d4a8a193
anushka958 <anushka.sapkal.aids.2023@vpkbiet.org>
2026-01-05 10:04:12 +0530

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,9 @@
+n=int(input("How many numbers?"))
+numbers[]
+for i in range(n):
+num=int(input("Enter numbers: "))
+numbers.append(num)
+print ("Odd numbers are: ")
+for num in numbers:
+if num%2 != 0:
+print(num)
